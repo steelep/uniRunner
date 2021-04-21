82a8e94987de91a86a04703bddfbb828e1d2b720
steelep <patricksteele.nz@gmail.com>
2021-04-21 16:19:39 +1200

    Add files via upload

--- smartContractProxy/migrations/1_deploy_contract.js
+++ smartContractProxy/migrations/1_deploy_contract.js
@@ -1,1 +1,9 @@
+let ArbProxy = artifacts.require("ArbProxy")
 
+module.exports = async function (deployer, network) {
+    try {
+        await deployer.deploy(ArbProxy)
+    } catch (e) {
+        console.log(`Error in migration: ${e.message}`)
+    }
+}
